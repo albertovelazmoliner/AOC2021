ac4d553520ebe7a11fb54699d21f44a0cf028763
Alberto Vélaz Moliner <albertovelaz.dev@gmail.com>
2021-12-07 14:39:13 +0100

    Update day7.js

--- day7.js
+++ day7.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-function cheapestFuel(data) {
+function cheapestFuel(data, isPartOne = true) {
     const crabPositions = data.split(',').map(number => parseInt(number)).sort((a, b) => a - b);
     let min = crabPositions[0];
     let max = crabPositions[crabPositions.length - 1];
@@ -8,8 +8,12 @@ function cheapestFuel(data) {
     for (let i = min; i <= max; i++) {
         let currentFuel = 0;
         for (let j = 0; j < crabPositions.length; j++) {
-            const steps = Math.abs(i - crabPositions[j]);
-            currentFuel += (steps * (steps + 1)) / 2;
+            if (isPartOne) {
+                currentFuel += Math.abs(i - crabPositions[j]);
+            } else {
+                const steps = Math.abs(i - crabPositions[j]);
+                currentFuel += (steps * (steps + 1)) / 2;
+            }
         }
         
         if (currentFuel < fuel) {
@@ -22,13 +26,21 @@ function cheapestFuel(data) {
     return fuel;
 }
 
-// TEST 1
+// TEST
 const dataTest = '16,1,2,0,4,2,7,1,2,14';
 
-function testCode(dataTest) {
-    const result = cheapestFuel(dataTest);
-    assert.equal(result, 168, 'test 1', 'Error in test')
+// TEST PART 1
+function testCode1(dataTest, isPartOne = true) {
+    const result = cheapestFuel(dataTest, isPartOne);
+    assert.equal(result, 37, 'test 1', 'Error in test 1')
     console.log('test 1 passed');
 }
+testCode1(dataTest);
 
-testCode(dataTest)
+// TEST PART 2
+function testCode2(dataTest, isPartOne = false) {
+    const result = cheapestFuel(dataTest, isPartOne);
+    assert.equal(result, 168, 'test 1', 'Error in test 2')
+    console.log('test 2 passed');
+}
+testCode2(dataTest);
